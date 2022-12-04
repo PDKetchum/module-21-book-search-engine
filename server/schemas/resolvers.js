59e149cdb2d730d4b50237d3c8d8b07f80b078b4
PDKetchum <pxiong265@gmail.com>
2022-12-04 17:13:21 -0600

    added me query

---
+++ server/schemas/resolvers.js
@@ -0,0 +1,17 @@
+const { AuthenticationError } = require("apollo-server-express");
+const { User, Book } = require("../models");
+const { signToken } = require("../utils/auth");
+
+const resolvers = {
+  Query: {
+    me: async (parent, args, context) => {
+      if (context.user) {
+        return User.findOne({ _id: context.user._id }).populate("books");
+      }
+      throw new AuthenticationError("You need to be logged in!");
+    },
+  },
+  Mutation: {},
+};
+
+module.exports = resolvers;
