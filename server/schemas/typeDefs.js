370168ce5f0649feba18eaf4346adac860cae4e2
PDKetchum <pxiong265@gmail.com>
2022-12-04 18:28:02 -0600

    add mutation type definition

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,6 +1,15 @@
 const { gql } = require("apollo-server-express");
 
 const typeDefs = gql`
+  input BookInput{
+    bookId: String
+    authors: [String]
+    description: String
+    title: String
+    image: String
+    link: String
+  }
+
   type User {
     _id: ID
     username: String
@@ -24,10 +33,14 @@ const typeDefs = gql`
   }
 
   type Query {
-    users: [User]
-    user(username: String!): User
-    savedBooks(username: String!): [Book]
     me: User
+  }
+
+  type Mutation{
+    login(email: String!, password: String!): Auth
+    addUser(username: String!, email: String!, password: String!): Auth
+    saveBook({input: BookInput}): User
+    removeBook(bookId: String!): User
   }
 `;
 
